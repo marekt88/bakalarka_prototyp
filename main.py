055f5e4618c49525eaebf24a2b12631e72ba6981
Marek Toth <marek.toth.3@student.tuke.sk>
2024-11-11 21:35:58 +0100

    Remove embedded repository tracking for frontend

---
+++ main.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, silero
+from api import AssistantFnc
+
+load_dotenv()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "Ste hlasový asistent vytvorený LiveKit. Vaše rozhranie s používateľmi bude hlasové."
+            "Mali by ste používať krátke a stručné odpovede a vyhýbať sa používaniu nevysloviteľnej interpunkcie."
+        ),
+    )
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
+
+    assistant = VoiceAssistant(
+        vad=silero.VAD.load(),
+        stt=openai.STT(),
+        llm=openai.LLM(),
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
+    )
+    assistant.start(ctx.room)
+
+    await asyncio.sleep(1)
+    await assistant.say("Hey, how can I help you today!", allow_interruptions=True)
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
